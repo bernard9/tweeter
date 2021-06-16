56d0433d18808ec2a3c95f4c7bcb26ff1f10e52d
Bernard Yang <ybernard@gmail.com>
2021-06-16 21:43:13 +0000

    implement timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  let time = $(".time-ago").attr("datetime");
+  let newTime = timeago.format(time);
+  $(".time-ago").text(newTime);
+});

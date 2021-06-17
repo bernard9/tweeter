81830f30fd86eaaa8a5892666fbfc01df1bdbb95
Bernard Yang <ybernard@gmail.com>
2021-06-17 02:57:51 +0000

    post to form changed to AJAX request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,52 +5,52 @@
  */
 
 $(document).ready(function() {
+
   const time = $(".time-ago").attr("datetime");
   const newTime = timeago.format(time);
   $(".time-ago").text(newTime);
-});
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  
-  // gets "past-tweets" section
-  const $tweets = $('.past-tweets');
-  for (const tweet of tweets) {
-    const newTweet = createTweetElement(tweet);
-    $tweets.append(newTweet);
-  }
-};
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
 
-const createTweetElement = function(tweet) {
-  let $tweet = $(`<article class="tweets">
+    // gets "past-tweets" section
+    const $tweets = $('.past-tweets');
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $tweets.append(newTweet);
+    }
+  };
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`<article class="tweets">
 <header>
   <div class="user-icon">
     <img id="man" src="/images/man.png"> 
@@ -68,7 +68,18 @@ const createTweetElement = function(tweet) {
   </div>
 </footer>
 </article>`);
-  return $tweet;
-};
+    return $tweet;
+  };
 
-renderTweets(data);
+  renderTweets(data);
+
+  $("form").on('submit', function(event) {
+    event.preventDefault();
+    const url = "/tweets";
+    $.ajax({
+      url: url,
+      method: "POST",
+      data: $(this).serialize(),
+    });
+  });
+});
